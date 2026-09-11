e0f7322fab597c4a472e141e23008f05f77cff14
David Oguns <davidoguns@gmail.com>
2026-09-11 13:13:30 -0700

    add: assertions on all message fields. Restructure test for message.

--- crates/aws-iot-securetunneling-localproxy/src/lib.rs
+++ crates/aws-iot-securetunneling-localproxy/src/lib.rs
@@ -13,6 +13,34 @@ pub fn version() -> &'static str {
 // may need editor/nvim to be restart if macro expansion starts to fail.
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/com.amazonaws.iot.securedtunneling.rs"));
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test_proto_message() {
+            use super::{message, Message};
+
+            // TODO: why aren't these fields optional?
+            let m = Message {
+                // r#type: proto::message::Type::from_str_name("StreamStart").unwrap(),
+                r#type: message::Type::StreamStart as i32,
+                stream_id: 2,
+                ignorable: false,
+                payload: vec![0x12u8, 0xffu8],
+                service_id: "".to_string(),
+                available_service_ids: vec!["SSH".to_string()],
+                connection_id: 0,
+            };
+
+            assert_eq!(m.r#type(), message::Type::StreamStart);
+            assert_eq!(m.stream_id, 2);
+            assert_eq!(m.ignorable, false);
+            assert_eq!(m.payload, vec![0x12u8, 0xffu8]);
+            assert_eq!(m.service_id, "".to_string());
+            assert_eq!(m.available_service_ids, vec!["SSH".to_string()]);
+            assert_eq!(m.connection_id, 0);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -22,28 +50,5 @@ mod tests {
     #[test]
     fn test_version() {
         assert!(!version().is_empty());
-    }
-
-    #[test]
-    fn test_proto_message() {
-        //basic check
-        use proto::{message, Message};
-
-        // TODO: why aren't these fields optional?
-        let m = Message {
-            // r#type: proto::message::Type::from_str_name("StreamStart").unwrap(),
-            r#type: message::Type::StreamStart as i32,
-            stream_id: 2,
-            ignorable: false,
-            payload: vec![0x12u8, 0xffu8],
-            service_id: "".to_string(),
-            available_service_ids: vec!["SSH".to_string()],
-            connection_id: 0,
-        };
-
-        assert_eq!(m.stream_id, 2);
-        assert_eq!(m.service_id, "".to_string());
-        assert_eq!(m.ignorable, false);
-        assert_eq!(m.connection_id, 0);
     }
 }
